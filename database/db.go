60ca7ac3c5e4558df9c9dc145358850f7a730753
ReygaFitra <regafitra12@gmail.com>
2023-04-15 09:51:54 +0700

    add db connection to credentials table

--- database/db.go
+++ database/db.go
@@ -1,17 +1,33 @@
-package authDelivery
+package database
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
 	authController "github.com/ReygaFitra/auth-jwt/controller"
 	authUtils "github.com/ReygaFitra/auth-jwt/utils"
 	"github.com/gin-gonic/gin"
-	// _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
-
-func AuthRouter() {
+func ConnectDB() {
+	dbHost := authUtils.DotEnv("DB_HOST")
+	dbPort := authUtils.DotEnv("DB_PORT")
+	dbUser := authUtils.DotEnv("DB_USER")
+	dbPassword := authUtils.DotEnv("DB_PASSWORD")
+	dbName := authUtils.DotEnv("DB_NAME")
+	sslMode := authUtils.DotEnv("SSL_MODE")
 	serverPort := authUtils.DotEnv("SERVER_PORT")
+
+	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", dbHost, dbPort, dbUser, dbPassword, dbName, sslMode)
+	db, err := sql.Open("postgres", dataSourceName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 
 	router := gin.Default()
 
